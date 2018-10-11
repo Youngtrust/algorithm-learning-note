c6f13951fd4e949e4abeca12d7343ba0a11122fd
khadgar <khadgaryoungtrust@gmail.com>
2018-10-11 13:29:01 -0400

    reverse single linked list

---
+++ Reverse SingleLL.java
@@ -0,0 +1,29 @@
+//Given the pointer/reference to the head of a singly linked list, reverse it and return the pointer/reference to the head of reversed linked list.
+
+class Reverse{
+    public static LinkedListNode reverse_recursive(LinkedListNode head) {
+      //no need to reverse if head is 
+      //null or there is only 1 node.
+      if (head == null || 
+          head.next == null) {
+          return head;
+      }
+  
+      LinkedListNode reversed_list = reverse_recursive(head.next);
+    
+        head.next.next = head;
+        head.next = null;
+        return reversed_list;
+    }
+    public static void main(String[] args) {
+      LinkedListNode list_head = null;
+      list_head = LinkedList.create_random_list(10);
+      
+      System.out.print("Original: ");
+      LinkedList.display(list_head);
+  
+      list_head = reverse_recursive(list_head);
+      System.out.print("After Reverse (Recursive):");
+      LinkedList.display(list_head);
+  }
+  }
