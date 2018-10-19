2c6d99ce8cb30da34f408ad12b8208ed756d9c6e
khadgar <khadgaryoungtrust@gmail.com>
2018-10-19 14:38:36 -0400

    Coin change

--- CoinChange.java
+++ CoinChange.java
@@ -18,43 +18,36 @@ You may assume that you have an infinite number of each kind of coin.
 F(S)=F(S−C)+1
 
 */
-
-public class Solution {
-
-    public int coinChange(int[] coins, int amount) {        
-        if (amount < 1) return 0;
-        return coinChange(coins, amount, new int[amount]);
-    }
-
-    private int coinChange(int[] coins, int rem, int[] count) {
-        if (rem < 0) return -1;
-        if (rem == 0) return 0;
-        if (count[rem - 1] != 0) return count[rem - 1];
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int res = coinChange(coins, rem - coin, count);
-            if (res >= 0 && res < min)
-                min = 1 + res;
-        }
-        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
-        return count[rem - 1];
-    }
-}
-
-
-public class Solution {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;             
-        int[] dp = new int[amount + 1];  
-        Arrays.fill(dp, max);  
-        dp[0] = 0;   
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
+        return first(coins, amount);
+    }
+
+    private int ans;
+    public int first(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return amount == 0 ? 1 : -1;
+        }
+        ans = amount + 1;
+        Arrays.sort(coins);
+        helper(coins, amount, 0, coins.length-1);
+        return ans == amount + 1 ? -1 : ans;
+    }
+
+
+    public void helper(int[] coins, int remain, int numCoins, int idx) {
+        int coin = coins[idx];
+        if (idx == 0) {
+            if (remain % coin == 0) {
+                this.ans = Math.min(this.ans, numCoins + remain / coin);
+            }
+        } else {
+            int k = remain / coin;
+            while (k >= 0 && numCoins + k < this.ans) {
+                /** remain will never be negative */
+                helper(coins, remain - k * coin, numCoins+k, idx-1);
+                k--;
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
