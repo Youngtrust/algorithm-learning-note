319f1b278ddae7d4f107db272277c146310a3d7f
khadgar <khadgaryoungtrust@gmail.com>
2018-10-19 10:40:07 -0400

    fastest way for 4 sum

--- 4Sum.java
+++ 4Sum.java
@@ -12,53 +12,41 @@ The solution set must not contain duplicate quadruplets.
     (-2,  0, 0, 2)
 
 */
-
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        return kSum(nums, 0, 4, target);
-    }
-    
-    private List<List<Integer>> kSum(int[] nums, int start, int k, int target){
-        int len = nums.length;
+    public List<List<Integer>> fourSum(int[] num, int target) {
         
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        
-        if(k == 2){
-            int left = start, right = len-1;
-            while(left < right){
-                int sum = nums[left] + nums[right];
-                if(sum == target){
-                    List<Integer> path = new ArrayList<Integer>();
-                    path.add(nums[left]);
-                    path.add(nums[right]);
-                    res.add(path);
-                    
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                    
-                }
-                else if(sum < target){
-                    left++;
-                }else{
-                    right--;
-                }
-            }
-            
-        }else{
-            for(int i = start; i < len - (k-1); i++){
-                if(i>start&&nums[i] == nums[i-1]) continue;
-                List<List<Integer>> temp = kSum(nums, i+1, k-1, target - nums[i]);
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+
+        if(num==null || num.length<4)
+            return ans;
+        Arrays.sort(num);
+
+        for(int i=0; i<num.length-3; i++){
+            if(num[i]+num[i+1]+num[i+2]+num[i+3]>target)break; //first candidate too large, search finished
+            if(num[i]+num[num.length-1]+num[num.length-2]+num[num.length-3]<target)continue; //first candidate too small
+            if(i>0&&num[i]==num[i-1])continue; //prevents duplicate result in ans list
+            for(int j=i+1; j<num.length-2; j++){
+                if(num[i]+num[j]+num[j+1]+num[j+2]>target)break; //second candidate too large
+                if(num[i]+num[j]+num[num.length-1]+num[num.length-2]<target)continue; //second candidate too small
+                if(j>i+1&&num[j]==num[j-1])continue; //prevents duplicate results in ans list
                 
-                for(List<Integer> t:temp){
-                    t.add(0, nums[i]);
+                
+                int low=j+1, high=num.length-1;
+                while(low<high){
+                    int sum=num[i]+num[j]+num[low]+num[high];
+                    if(sum==target){
+                        ans.add(Arrays.asList(num[i], num[j], num[low], num[high]));
+                        while(low<high&&num[low]==num[low+1])low++; //skipping over duplicate on low
+                        while(low<high&&num[high]==num[high-1])high--; //skipping over duplicate on high
+                        low++; 
+                        high--;
+                    }
+                    //move window
+                    else if(sum<target)low++; 
+                    else high--;
                 }
-                res.addAll(temp);
             }
         }
-        return res;
+        return ans;
     }
 }
