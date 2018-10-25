8142a04569266150102df9dfd673621cb93163e0
khadgar <khadgaryoungtrust@gmail.com>
2018-10-25 10:21:19 -0400

    how to bs in java

--- bs.java
+++ bs.java
@@ -4,6 +4,12 @@ public class Solution {
      * @param target: An integer
      * @return: An integer
      */
+
+    //start + 1 < end;
+    //start + (end - start)/2;
+    // nums[mid]  == < >
+    //nums[start], nums[end] ? target
+    
     public int fbinarySearch(int[] nums, int target) {
         // write your code here
         if(nums.length == 0){
@@ -12,6 +18,8 @@ public class Solution {
         int start = 0;
         int end = nums.length - 1;
         int mid;
+
+        //start + 1 < end
         while(start + 1 < end){
             mid = start + (end - start)/2;
             if(nums[mid] == target){
@@ -23,6 +31,7 @@ public class Solution {
             }
         }
 
+        // asked to find 1st position; if start first
         if(nums[start] == target){
             return start;
         }if(nums[end] == target){
