80e41c5c414834d705720b472b71689399e86b9f
khadgar <khadgaryoungtrust@gmail.com>
2018-11-06 11:14:46 -0500

    single number

--- Math/SingleNumber.java
+++ Math/SingleNumber.java
@@ -16,9 +16,6 @@ Output: 4
 */
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length <= 1){
-            return nums[0];
-        }
         
         int result = 0;
         for(int i = 0; i < nums.length; i++){
