3f9ccb357777ebaa438e45d1d86347356f3e4182
khadgar <khadgaryoungtrust@gmail.com>
2018-11-06 11:17:36 -0500

    single NUmber II

--- Math/SingleNumberII.java
+++ Math/SingleNumberII.java
@@ -15,3 +15,13 @@ Input: [0,1,0,1,0,1,99]
 Output: 99
 */
 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int i = 0; i < nums.length; i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
+        }
+}
